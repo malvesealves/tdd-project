ad86221b3e805f676f1cbe343b11a77a7788349c
Mateus Alves <m.alvesealves@gmail.com>
2025-02-09 09:37:32 -0300

    Adding service extensions to Program.cs

---
+++ ECommerce.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Restaurants.Api.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors((opt) =>
+            {
+                opt.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
+        }
+
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen((options) =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "TDD Project - Restaurants API",
+                    Description = "An ASP.NET Core Web API for demonstrating TDD strategy",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Example Contact",
+                        Url = new Uri("https://example.com/contact")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Example License",
+                        Url = new Uri("https://example.com/license")
+                    },
+                });
+            });
+        }
+    }
+}
--- ECommerce.Api/Program.cs
+++ ECommerce.Api/Program.cs
@@ -1,8 +1,12 @@
+using Restaurants.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.ConfigureCors();
+builder.Services.ConfigureSwagger();
 
 var app = builder.Build();
 
